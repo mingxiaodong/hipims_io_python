90597ebae989d8dd7bc84438be4e8a8c8e2f6cd3
Xiaodong Ming <xiaodong.ming@outlook.com>
2020-04-01 16:02:29 +0100

    write setup details

--- setup.py
+++ setup.py
@@ -12,12 +12,27 @@ Created on Wed Apr  1 15:03:58 2020
 
 from setuptools import setup
 
-setup(name='funniest',
+setup(name='hipims_io',
       version='0.1',
-      description='The funniest joke in the world',
-      url='http://github.com/storborg/funniest',
-      author='Flying Circus',
-      author_email='flyingcircus@example.com',
-      license='MIT',
-      packages=['funniest'],
+      description='To process input and output files of the HiPIMS model',
+      url='https://github.com/mingxiaodong/hipims_io_py_package',
+      author='Xiaodong Ming',
+      author_email='xiaodong.ming@outlook.com',
+      license='NCL',
+      packages=['hipims_io'],
+      install_requires=['gdal',],
       zip_safe=False)
+
+"""
+#Sometimes you’ll want to use packages that are properly arranged with 
+#setuptools, but aren’t published to PyPI. In those cases, you can specify a 
+#list of one or more dependency_links URLs where the package can be downloaded,
+#along with some additional hints, and setuptools will find and install the
+#package correctly.
+#For example, if a library is published on GitHub, you can specify it like:
+setup(
+    ...
+    dependency_links=['http://github.com/user/repo/tarball/master#egg=package-1.0']
+    ...
+)
+"""
